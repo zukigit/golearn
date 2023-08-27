254aab87da2296cd112b38f767101bb6920f8b1c
zuki <send2zuki@gmail.com>
2023-08-27 21:31:32 +0630

    added mutexs

--- lvl2/package2/mutexesTest.go
+++ lvl2/package2/mutexesTest.go
@@ -1,8 +1,42 @@
 package package2
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type Container struct{
+	mu sync.Mutex
+	counters map[string]int
+}
+
+func (c *Container) increase(name string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.counters[name]++
+}
 
 func MutexesTest(msg string) {
 	fmt.Println(msg)
 
+	ctn := Container{
+		counters: map[string]int{"sell": 0, "buy": 0},
+	}
+	var wg sync.WaitGroup
+
+	doIncresement := func(name string, count int) {
+		for i := 0; i < count; i++ {
+			ctn.increase(name)
+		}
+		wg.Done()
+	}
+
+	wg.Add(4)
+	doIncresement("sell", 200)
+	doIncresement("sell", 500)
+	doIncresement("buy", 800)
+	doIncresement("buy", 100)
+
+	wg.Wait()
+	fmt.Println(ctn.counters)
 }
